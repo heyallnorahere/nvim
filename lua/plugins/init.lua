99259664fdb74aa6e508c0d31d12a550952bfeea
heyallnorahere <nora.github@bedafamily.com>
2025-11-06 14:18:27 -0800

    add discord rpc

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,3 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -17,7 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("config.set")
 
--- Setup lazy.nvim
 require("lazy").setup({
     spec = {
         -- lsp
@@ -77,31 +75,11 @@ require("lazy").setup({
             },
             config = function()
                 require("nvim-treesitter.configs").setup({
-                    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
                     ensure_installed = { "c", "glsl", "cpp", "glsl", "yaml", "bash", "cmake", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-
-                    -- Install parsers synchronously (only applied to `ensure_installed`)
                     sync_install = false,
-
-                    -- Automatically install missing parsers when entering buffer
-                    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
                     auto_install = true,
-
-                    -- List of parsers to ignore installing (or "all")
-                    --ignore_install = { "javascript" },
-
-                    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-                    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
                     highlight = {
                         enable = true,
-
-                        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-                        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-                        -- the name of the parser)
-                        -- list of language that will be disabled
-                        --disable = { "c", "rust" },
-                        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
                         disable = function(lang, buf)
                             local max_filesize = 100 * 1024 -- 100 KB
                             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -109,11 +87,6 @@ require("lazy").setup({
                                 return true
                             end
                         end,
-
-                        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                        -- Instead of true it can also be a list of languages
                         additional_vim_regex_highlighting = true,
                     },
                 })
@@ -205,11 +178,35 @@ require("lazy").setup({
                 "mfussenegger/nvim-dap",
             },
         },
+
+        -- discord rpc
+        {
+            "andweeb/presence.nvim",
+            event = "VeryLazy",
+            config = function()
+                local presence = {
+                    auto_update = true,
+                    neovim_image_text = "nyoravim",
+                    main_image = "neovim",
+                    log_level = nil,
+                    show_time = false,
+                    enable_line_number = false,
+                    buttons = true,
+
+                    editing_text = "editing %s",
+                    file_explorer_text = "browsing %s",
+                    git_commit_text = "committing",
+                    plugin_manager_text = "managing plugins",
+                    reading_text = "viewing %s",
+                    workspace_text = "working on %s",
+                    line_number_text = "%s/%s",
+                }
+
+                require("presence").setup(presence)
+            end,
+        },
     },
 
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "catppuccin" } },
-    -- automatically check for plugin updates
     checker = { enabled = true },
 })
